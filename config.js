6c3ebe9a08f0fcf013429824ee186fc4453bdf30
Caseyrhodes01 <palvinrhodes@gmail.com>
2024-09-20 19:02:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 //NOTE : cara menjadi publik owner silahkan Pergi ke file; all/database/owner.json
 
 require("./all/module")
-global.owner = "2348102487241" 
-global.namaCreator = "єdnut hímsєlf" 
+global.owner = "254112192119" 
+global.namaCreator = "𝐜𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐝 hímsєlf" 
 global.autoJoin = false
 global.antilink = false 
 global.versisc = '1.0' 
@@ -12,10 +12,10 @@ global.codeInvite = ""
 global.isLink = ' ' 
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "naruto bot" 
-global.author = "YT :Ednut001" 
+global.packname = "𝐜𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐬 bot" 
+global.author = "YT :𝐜𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐬" 
 global.jumlah = "5" 
-global.namabot = "Naruto Bot crash" 
+global.namabot = "𝐜𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐬 Bot crash" 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
